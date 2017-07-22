2168323c5a029096fdcae9ebcd8e09b2eb1fec74
Trayle <hsommerhoff@aol.com>
2017-07-22 02:11:44 +0200

    Add window creation

--- src/maze.cpp
+++ src/maze.cpp
@@ -1,7 +1,70 @@
 #include <iostream>
+#include <QApplication>
+#include <QKeyEvent>
+
+#include <qvr/manager.hpp>
+
 #include "maze.h"
 
-int main()
+Maze::Maze()
 {
-  std::cout << "Hello World" << std::endl;
+
+}
+
+bool Maze::initProcess(QVRProcess* p)
+{
+	initializeOpenGLFunctions();
+
+	return true;
+}
+
+void Maze::render(QVRWindow* w,
+				  const QVRRenderContext& context,
+				  int viewPass,
+				  unsigned int texture)
+{
+
+}
+
+void Maze::update(const QList<QVRObserver*>& observers)
+{
+
+}
+
+bool Maze::wantExit()
+{
+	return false;
+}
+
+void Maze::serializeDynamicData(QDataStream& ds) const
+{
+
+}
+
+void Maze::deserializeDynamicData(QDataStream& ds)
+{
+
+}
+
+void Maze::keyPressEvent(const QVRRenderContext& context, QKeyEvent* event)
+{
+
+}
+int main(int argc, char* argv[])
+{
+	QApplication app(argc, argv);
+	QVRManager manager(argc, argv);
+
+	QSurfaceFormat format;
+	format.setProfile(QSurfaceFormat::CoreProfile);
+	format.setVersion(3, 3);
+	QSurfaceFormat::setDefaultFormat(format);
+
+	Maze qvrapp;
+	if (!manager.init(&qvrapp)) {
+		qCritical("Cannot initialize QVR manager");
+		return 1;
+	}
+
+	return app.exec();
 }
--- src/maze.h
+++ src/maze.h
@@ -1,9 +1,37 @@
-#ifndef MAZE_INCLUDE_MAZE_H
-#define MAZE_INCLUDE_MAZE_H
+#ifndef MAZE_MAZE_H
+#define MAZE_MAZE_H
 
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLShaderProgram>
+#include <QElapsedTimer>
 
+#include <qvr/app.hpp>
 
+class Maze : public QVRApp, protected QOpenGLFunctions_3_3_Core
+{
+private:
 
+protected:
+public:
+	Maze();
 
+	bool initProcess(QVRProcess* p) override;
 
-#endif  // MAZE_INCLUDE_MAZE_H
+	void render(QVRWindow* w,
+				const QVRRenderContext& context,
+				int viewPass,
+				unsigned int texture)
+		override;
+
+	void update(const QList<QVRObserver*>& observers) override;
+
+	bool wantExit() override;
+
+	void serializeDynamicData(QDataStream& ds) const override;
+	void deserializeDynamicData(QDataStream& ds) override;
+
+	void keyPressEvent(const QVRRenderContext& context, QKeyEvent* event) override;
+
+};
+
+#endif  // MAZE_MAZE_H
