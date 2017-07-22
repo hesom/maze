528ac69b2d1ea418c5648a2bf43e2b5b3f2f0f85
Trayle <hsommerhoff@aol.com>
2017-07-22 02:10:55 +0200

    Add mesh class

---
+++ src/mesh.cpp
@@ -0,0 +1,46 @@
+
+#include "mesh.h"
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices)
+{
+	this->_vertices = vertices;
+	this->_indices = indices;
+	setupMesh();
+}
+
+void Mesh::setupMesh()
+{
+	glGenVertexArrays(1, &_vao);
+	glBindVertexArray(_vao);
+
+	glGenBuffers(1, &_vbo);
+	glGenBuffers(1, &_ebo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+	glBufferData(GL_ARRAY_BUFFER, _vertices.size() * sizeof(Vertex), _vertices.data(), GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, _indices.size() * sizeof(unsigned int), _indices.data(), GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_TRUE, sizeof(Vertex), (void*)offsetof(Vertex, color));
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+
+
+void Mesh::draw()
+{
+	glBindVertexArray(_vao);
+	glDrawElements(GL_TRIANGLES, _indices.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+}
---
+++ src/mesh.h
@@ -0,0 +1,31 @@
+#ifndef MAZE_MESH_H
+#define MAZE_MESH_H
+
+#include <QVector3D>
+#include <QOpenGLFunctions_3_3_Core>
+#include <vector>
+
+struct Vertex
+{
+	QVector3D position;
+	QVector3D normal;
+	QVector3D color;
+};
+
+class Mesh : QOpenGLFunctions_3_3_Core
+{
+private:
+	std::vector<Vertex> _vertices;
+	std::vector<unsigned int> _indices;
+	GLuint _vao;
+	GLuint _vbo;
+	GLuint _ebo;
+
+	void setupMesh();
+public:
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices);
+	void draw();
+};
+
+#endif // MAZE_MESH_H
+
