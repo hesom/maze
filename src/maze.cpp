9923e5920b29639288a7e398ac353777a8d85f74
Trayle <hsommerhoff@aol.com>
2017-07-22 21:25:00 +0200

    Move cube mesh initialization to Mesh class

--- src/maze.cpp
+++ src/maze.cpp
@@ -3,6 +3,7 @@
 #include <QKeyEvent>
 
 #include <qvr/manager.hpp>
+#include <qvr/observer.hpp>
 
 #include "maze.h"
 
@@ -17,56 +18,7 @@ bool Maze::initProcess(QVRProcess* p)
 {
 	initializeOpenGLFunctions();
 
-	float pos[] = {
-		-1.0f, +1.0f, +1.0f,   +1.0f, +1.0f, +1.0f,   +1.0f, -1.0f, +1.0f,   -1.0f, -1.0f, +1.0f, // front
-		-1.0f, +1.0f, -1.0f,   +1.0f, +1.0f, -1.0f,   +1.0f, -1.0f, -1.0f,   -1.0f, -1.0f, -1.0f, // back
-		+1.0f, +1.0f, -1.0f,   +1.0f, +1.0f, +1.0f,   +1.0f, -1.0f, +1.0f,   +1.0f, -1.0f, -1.0f, // right
-		-1.0f, +1.0f, -1.0f,   -1.0f, +1.0f, +1.0f,   -1.0f, -1.0f, +1.0f,   -1.0f, -1.0f, -1.0f, // left
-		-1.0f, +1.0f, +1.0f,   +1.0f, +1.0f, +1.0f,   +1.0f, +1.0f, -1.0f,   -1.0f, +1.0f, -1.0f, // top
-		-1.0f, -1.0f, +1.0f,   +1.0f, -1.0f, +1.0f,   +1.0f, -1.0f, -1.0f,   -1.0f, -1.0f, -1.0f  // bottom
-	};
-
-	float n[] = {
-		0.0f, 0.0f, +1.0f,   0.0f, 0.0f, +1.0f,   0.0f, 0.0f, +1.0f,   0.0f, 0.0f, +1.0f, // front
-		0.0f, 0.0f, -1.0f,   0.0f, 0.0f, -1.0f,   0.0f, 0.0f, -1.0f,   0.0f, 0.0f, -1.0f, // back
-		+1.0f, 0.0f, 0.0f,   +1.0f, 0.0f, 0.0f,   +1.0f, 0.0f, 0.0f,   +1.0f, 0.0f, 0.0f, // right
-		-1.0f, 0.0f, 0.0f,   -1.0f, 0.0f, 0.0f,   -1.0f, 0.0f, 0.0f,   -1.0f, 0.0f, 0.0f, // left
-		0.0f, +1.0f, 0.0f,   0.0f, +1.0f, 0.0f,   0.0f, +1.0f, 0.0f,   0.0f, +1.0f, 0.0f, // top
-		0.0f, -1.0f, 0.0f,   0.0f, -1.0f, 0.0f,   0.0f, -1.0f, 0.0f,   0.0f, -1.0f, 0.0f  // bottom
-	};
-
-	unsigned int index[] = {
-		0, 3, 1, 1, 3, 2, // front face
-		4, 5, 7, 5, 6, 7, // back face
-		8, 9, 11, 9, 10, 11, // right face
-		12, 15, 13, 13, 15, 14, // left face
-		16, 17, 19, 17, 18, 19, // top face
-		20, 23, 21, 21, 23, 22, // bottom face
-	};
-
-	std::vector<QVector3D> positions;
-	std::vector<QVector3D> normals;
-	std::vector<unsigned int> indices;
-
-	for (int i = 0; i < 72; i+=3) {
-		positions.push_back(QVector3D(pos[i], pos[i + 1], pos[i + 2]));
-		normals.push_back(QVector3D(n[i], n[i + 1], n[i + 2]));
-	}
-
-	for (int i = 0; i < 36; ++i) {
-		indices.push_back(index[i]);
-	}
-
-	std::vector<Vertex> vertices;
-	for (int i = 0; i < positions.size(); ++i) {
-		Vertex v;
-		v.position = positions[i];
-		v.normal = normals[i];
-		vertices.push_back(v);
-	}
-
-	_cube = std::make_shared<Mesh>(vertices, indices);
-
+	_cube = Mesh::cubeMesh();
 
 	glGenFramebuffers(1, &_fbo);
 	glBindFramebuffer(GL_FRAMEBUFFER, _fbo);
@@ -116,9 +68,19 @@ void Maze::render(QVRWindow* w,
 	_prg.setUniformValue("color", color);
 
 	QMatrix4x4 modelMatrix;
-	modelMatrix.scale(0.5);
-	modelMatrix.translate(2.0f, 0.0f, -25.0f);
+	modelMatrix.translate(0.0f, 0.0f, -10.0f);
+	modelMatrix.scale(0.1f);
+	modelMatrix.translate(0.0f, 0.5f, 0.0f);
 	QMatrix4x4 modelViewMatrix = viewMatrix * modelMatrix;
+	_prg.setUniformValue("modelview_matrix", modelViewMatrix);
+	_prg.setUniformValue("normal_matrix", modelViewMatrix.normalMatrix());
+	_cube->draw();
+
+	modelMatrix.setToIdentity();
+	modelMatrix.translate(0.0f, 0.1f, -10.0f);
+	modelMatrix.scale(0.1f);
+	modelMatrix.translate(0.0f, 0.5f, 0.0f);
+	modelViewMatrix = viewMatrix * modelMatrix;
 	_prg.setUniformValue("modelview_matrix", modelViewMatrix);
 	_prg.setUniformValue("normal_matrix", modelViewMatrix.normalMatrix());
 	_cube->draw();
@@ -138,6 +100,12 @@ void Maze::update(const QList<QVRObserver*>& observers)
 			float seconds = (_elapsedTime + _timer.elapsed()) / 1000.0f;
 			//Animation here
 		}
+	}
+	for (auto obs : observers) {
+		QVector3D pos = obs->navigationPosition();
+		QVector3D eye = obs->trackingPosition();
+		std::cout << "Nav: (" << pos.x() << "," << pos.y() <<"," << pos.z() << ")"<< std::endl;
+		std::cout << "Tracking: (" << eye.x() << "," << eye.y() << "," << eye.z() << ")" << std::endl;
 	}
 }
 
--- src/mesh.h
+++ src/mesh.h
@@ -1,6 +1,7 @@
 #ifndef MAZE_MESH_H
 #define MAZE_MESH_H
 
+#include <memory>
 #include <QVector3D>
 #include <QOpenGLFunctions_3_3_Core>
 #include <vector>
@@ -25,6 +26,8 @@ private:
 public:
 	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices);
 	void draw();
+
+	static std::shared_ptr<Mesh> cubeMesh();
 };
 
 #endif // MAZE_MESH_H
