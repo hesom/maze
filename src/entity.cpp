33ce11649bdad039c5aa97d5d07bc9e0cb9cc688
Trayle <hsommerhoff@aol.com>
2017-07-22 03:14:13 +0200

    Add entity class

---
+++ src/entity.cpp
@@ -0,0 +1,16 @@
+
+#include "entity.h"
+
+Entity::Entity(std::shared_ptr<Mesh> mesh, QVector3D position, QVector3D scale, QVector3D rotation)
+{
+	this->_mesh = mesh;
+	this->_position = position;
+	this->_scale = scale;
+	this->_rotation = rotation;
+}
+
+Entity::Entity(std::shared_ptr<Mesh> mesh, QVector3D position, float scale, QVector3D rotation)
+{
+	QVector3D s(scale, scale, scale);
+	Entity(mesh, position, s, rotation);
+}
---
+++ src/entity.h
@@ -0,0 +1,25 @@
+#ifndef MAZE_ENTITY_H
+#define MAZE_ENTITY_H
+
+#include <memory>
+#include <QVector3D>
+#include <QMatrix4x4>
+#include <QOpenGLFunctions_3_3_Core>
+#include "mesh.h"
+
+//Drawable object
+class Entity : QOpenGLFunctions_3_3_Core
+{
+private:
+	std::shared_ptr<Mesh> _mesh;
+	QVector3D _position;
+	QVector3D _scale;
+	QVector3D _rotation;
+
+public:
+	Entity(std::shared_ptr<Mesh> mesh,  QVector3D position, QVector3D scale, QVector3D rotation);
+	Entity(std::shared_ptr<Mesh> mesh, QVector3D position, float scale, QVector3D rotation);
+};
+
+#endif // MAZE_OBJECT_H
+
